06b468c0b7fddaa0d6f76a3c635bb508a04b6a1e
RoUchiha <swaggsikh@gmail.com>
2022-07-23 21:38:09 -0500

    Sidebar Component done for now. Working on Navbar.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { FiSettings } from 'react-icons/fi'
 import { Tooltip, TooltipComponent } from '@syncfusion/ej2-react-popups'
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor} from './pages'
+import { Ecommerce, Line, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor} from './pages'
+
+import { useStateContext } from './contexts/ContextProvider';
+
 
 import './App.css'
 
 const App = () => {
 
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
